3bd088a430be3be1d22253c624a911eaec31f4cf
ouaddioussama <oussama.ouaddi@gmail.com>
2023-05-09 00:17:46 +0200

    add getConvertedNumber to foobarquixService

--- foobarquix-ui/src/app/foo-bar-quix.service.ts
+++ foobarquix-ui/src/app/foo-bar-quix.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FooBarQuixService {
-  constructor() { }
+
+  rootUrl='http://localhost:8080/foo-bar-quix/' ;
+  private httpOptions = {
+  headers: new HttpHeaders( { 'Content-Type': 'application/json' })
+ };
+  httpClient: any;
+
+
+  constructor(private http: HttpClient) { }
+
+  getConvertedNumber(inputNumber:number) {
+    return this.http.get(this.rootUrl+inputNumber);
+  }
 
 }
